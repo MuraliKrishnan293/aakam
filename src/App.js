891354383b7e1897f5eb7b1e34262c387090dce6
MuraliKrishnan <muralikrishnan.cse293@gmail.com>
2024-12-05 23:53:48 +0530

    revert

--- src/App.js
+++ src/App.js
@@ -1,98 +1,3 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-// import Nav from "./Components/Nav";
-// import { useEffect, useState } from "react";
-// import Land from "./Components/Land";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import About from "./Components/About";
-// import { ContactUs } from "./Components/Contact";
-// import Gallery from "./Components/Gallery";
-// import AOS from 'aos';
-// import 'aos/dist/aos.css';
-// import ReactWhatsapp from 'react-whatsapp';
-// import { FaWhatsapp } from 'react-icons/fa';
-// import Admin from './Components/Admin';
-// import AdminPanel from './Components/AdminPanel';
-// import Kit from "./Components/Kit";
-// import { Popup } from "./Components/Popup";
-
-// function App() {
-//   const [load, setLoad] = useState(true);
-//   const [isPopupVisible, setIsPopupVisible] = useState(false);
-
-//   useEffect(() => {
-//     AOS.init();
-//     setTimeout(() => {
-//       setLoad(false);
-//     }, 3000);
-
-//     // const handle = ()=>{
-//       const popupShown = sessionStorage.getItem('popupShown');
-//     // }
-    
-    
-//     if (!popupShown) {
-//       const popupTimer = setTimeout(() => {
-//         setIsPopupVisible(true);
-//         sessionStorage.setItem('popupShown', 'true'); // Mark the popup as shown
-//       }, 10000);
-
-//     return () => clearTimeout(popupTimer);
-//     }
-//   }, []);
-
-//   const handle = ()=>{
-//     const popupShown = sessionStorage.getItem('popupShown');
-//   }
-
-//   const popupShown = sessionStorage.getItem('popupShown');
-
-//   return (
-//     <div className="App">
-//       {load ? (
-//         <div className="loading-screen">
-//           <div className="spinner"></div>
-//         </div>
-//       ) : (
-//         <>
-//         <BrowserRouter>
-//         <Nav />
-//           <Routes>
-//             <Route path='/' element={<Land />} />
-//             <Route path='/about' element={<About />} />
-//             <Route path='/contact' element={<ContactUs />} />
-//             <Route path="/gallery" element={<Gallery />} />
-//             <Route path="/admin" element={<Admin />} />
-//             <Route path="/admin-panel" element={<AdminPanel />} />
-//             <Route path="/check" element={<Kit />} />
-//             <Route path="*" element={<h1 style={{marginTop: "200px"}}>404 - Not Found</h1>} />
-//           </Routes>
-//         </BrowserRouter>
-        
-//         {/* <ReactWhatsapp number="9884624057" message="Hello World!!!" /> */}
-//         <a href={`https://wa.me/918056491710?text=Hello`} target="_blank" rel="noopener noreferrer">
-//             <div className="whatsapp-button">
-//               <FaWhatsapp className="whatsapp-icon" />
-//             </div>
-//           </a>
-        
-//         {/* Popup */}
-//         { !popupShown && (<Popup
-//             isVisible={isPopupVisible}
-//             onClick={handle}
-//             onClose={() => setIsPopupVisible(false)}
-//           />)}
-//       </>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
 import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./Components/Nav";
@@ -124,11 +29,11 @@ function App() {
         setTimeout(() => {
           localStorage.clear();
           alert("Session expired. Please log in again.");
-          nav('/admin');
+          window.location.href = '/admin';
         }, remainingTime);
       } else {
         localStorage.clear();
-        alert("Session expired. Please log in again.");
+        // alert("Session expired. Please log in again.");
         // nav('/admin');
       }
     }
